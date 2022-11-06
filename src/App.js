17144e9b6e952e9a08f4ca993ea42bc5c06242ed
Zach Stone <zachinjapan888@gmail.com>
2022-11-05 21:53:08 -0400

    add wikapedia toast

--- src/App.js
+++ src/App.js
@@ -5,58 +5,85 @@ import { useWindowSize } from "@react-hook/window-size";
 import Confetti from "react-confetti";
 import correctSound from "../src/audio/correct.m4a";
 import errorSound from "../src/audio/error.m4a";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+
 
 // outside of function so
 const cardImages = [
-  { src: "/images/aomori.jpg", matched: false, alt: "aomori" },
-  { src: "/images/aichi.jpg", matched: false, alt: "aichi" },
-  { src: "/images/akita.jpg", matched: false, alt: "akita" },
-  { src: "/images/aomori.jpg", matched: false, alt: "aomori" },
-  { src: "/images/chiba.jpg", matched: false, alt: "chiba" },
-  { src: "/images/Ehime.jpg", matched: false, alt: "ehime" },
-  { src: "/images/fukui.jpg", matched: false, alt: "fukui" },
-  { src: "/images/fukuoka.jpg", matched: false, alt: "fukuoka" },
-  { src: "/images/fukushima.jpg", matched: false, alt: "fukushima" },
-  { src: "/images/gifu.jpg", matched: false, alt: "gifu" },
-  { src: "/images/gunma.jpg", matched: false, alt: "gunma" },
-  { src: "/images/hiroshima.png", matched: false, alt: "hiroshima" },
-  { src: "/images/hokkaido.jpg", matched: false, alt: "hokkaido" },
-  { src: "/images/hyogo.jpg", matched: false, alt: "hyogo" },
-  { src: "/images/ibaraki.jpg", matched: false, alt: "ibaraki" },
-  { src: "/images/ishikawa.jpg", matched: false, alt: "ishikawa" },
-  { src: "/images/iwate.jpg", matched: false, alt: "iwate" },
-  { src: "/images/kagawa.jpg", matched: false, alt: "kagawa" },
-  { src: "/images/kagoshima.jpg", matched: false, alt: "kagoshima" },
-  { src: "/images/kanagawa.jpg", matched: false, alt: "kanagawa" },
-  { src: "/images/kochi.jpg", matched: false, alt: "kochi" },
-  { src: "/images/kumamoto.jpg", matched: false, alt: "kumamoto" },
-  { src: "/images/kyoto.jpg", matched: false, alt: "kyoto" },
-  { src: "/images/miyagi.jpg", matched: false, alt: "miyagi" },
-  { src: "/images/mie.jpg", matched: false, alt: "mie" },
-  { src: "/images/miyazaki.jpg", matched: false, alt: "miyazaki" },
-  { src: "/images/nagano.jpg", matched: false, alt: "nagano" },
-  { src: "/images/nagasaki.jpg", matched: false, alt: "nagasaki" },
-  { src: "/images/nara.jpg", matched: false, alt: "nara" },
-  { src: "/images/niigata.jpg", matched: false, alt: "niigata" },
-  { src: "/images/oita.jpg", matched: false, alt: "oita" },
-  { src: "/images/okayama.jpg", matched: false, alt: "okayama" },
-  { src: "/images/okinawa.jpg", matched: false, alt: "okinawa" },
-  { src: "/images/osaka.jpg", matched: false, alt: "osaka" },
-  { src: "/images/saga.jpg", matched: false, alt: "saga" },
-  { src: "/images/saitama.jpg", matched: false, alt: "saitama" },
-  { src: "/images/shiga.jpg", matched: false, alt: "shiga" },
-  { src: "/images/shimane.jpg", matched: false, alt: "shimane" },
-  { src: "/images/shizuoka.jpg", matched: false, alt: "shizuoka" },
-  { src: "/images/tochigi.jpg", matched: false, alt: "tochigi" },
-  { src: "/images/tokushima.jpg", matched: false, alt: "tokushima" },
-  { src: "/images/tokyo.jpg", matched: false, alt: "tokyo" },
-  { src: "/images/tottori.jpg", matched: false, alt: "tottori" },
-  { src: "/images/toyama.jpg", matched: false, alt: "toyama" },
-  { src: "/images/wakayama.jpg", matched: false, alt: "wakayama" },
-  { src: "/images/yamagata.jpg", matched: false, alt: "yamagata" },
-  { src: "/images/yamaguchi.jpg", matched: false, alt: "yamaguchi" },
-  { src: "/images/yamanashi.jpg", matched: false, alt: "yamanashi" },
+  { src: "/images/aomori.jpg", matched: false, alt: "Aomori_Prefecture" },
+  { src: "/images/aichi.jpg", matched: false, alt: "Aichi_Prefecture" },
+  { src: "/images/akita.jpg", matched: false, alt: "Akita_Prefecture" },
+  { src: "/images/aomori.jpg", matched: false, alt: "Aomori_Prefecture" },
+  { src: "/images/chiba.jpg", matched: false, alt: "Chiba_Prefecture" },
+  { src: "/images/Ehime.jpg", matched: false, alt: "Ehime_Prefecture" },
+  { src: "/images/fukui.jpg", matched: false, alt: "Fukui_Prefecture" },
+  { src: "/images/fukuoka.jpg", matched: false, alt: "Fukuoka_Prefecture" },
+  { src: "/images/fukushima.jpg", matched: false, alt: "Fukushima_Prefecture" },
+  { src: "/images/gifu.jpg", matched: false, alt: "Gifu_Prefecture" },
+  { src: "/images/gunma.jpg", matched: false, alt: "Gunma_Prefecture" },
+  { src: "/images/hiroshima.png", matched: false, alt: "Hiroshima_Prefecture" },
+  { src: "/images/hokkaido.jpg", matched: false, alt: "Hokkaido_Prefecture" },
+  { src: "/images/hyogo.jpg", matched: false, alt: "Hyogo_Prefecture" },
+  { src: "/images/ibaraki.jpg", matched: false, alt: "Ibaraki_Prefecture" },
+  { src: "/images/ishikawa.jpg", matched: false, alt: "Ishikawa_Prefecture" },
+  { src: "/images/iwate.jpg", matched: false, alt: "Iwate_Prefecture" },
+  { src: "/images/kagawa.jpg", matched: false, alt: "Kagawa_Prefecture" },
+  { src: "/images/kagoshima.jpg", matched: false, alt: "Kagoshima_Prefecture" },
+  { src: "/images/kanagawa.jpg", matched: false, alt: "Kanagawa_Prefecture" },
+  { src: "/images/kochi.jpg", matched: false, alt: "Kochi_Prefecture" },
+  { src: "/images/kumamoto.jpg", matched: false, alt: "Kumamoto_Prefecture" },
+  { src: "/images/kyoto.jpg", matched: false, alt: "Kyoto_Prefecture" },
+  { src: "/images/miyagi.jpg", matched: false, alt: "Miyagi_Prefecture" },
+  { src: "/images/mie.jpg", matched: false, alt: "Mie_Prefecture" },
+  { src: "/images/miyazaki.jpg", matched: false, alt: "Miyazaki_Prefecture" },
+  { src: "/images/nagano.jpg", matched: false, alt: "Nagano_Prefecture" },
+  { src: "/images/nagasaki.jpg", matched: false, alt: "Nagasaki_Prefecture" },
+  { src: "/images/nara.jpg", matched: false, alt: "Nara_Prefecture" },
+  { src: "/images/niigata.jpg", matched: false, alt: "Niigata_Prefecture" },
+  { src: "/images/oita.jpg", matched: false, alt: "Oita_Prefecture" },
+  { src: "/images/okayama.jpg", matched: false, alt: "Okayama_Prefecture" },
+  { src: "/images/okinawa.jpg", matched: false, alt: "Okinawa_Prefecture" },
+  { src: "/images/osaka.jpg", matched: false, alt: "Osaka_Prefecture" },
+  { src: "/images/saga.jpg", matched: false, alt: "Saga_Prefecture" },
+  { src: "/images/saitama.jpg", matched: false, alt: "Saitama_Prefecture" },
+  { src: "/images/shiga.jpg", matched: false, alt: "Shiga_Prefecture" },
+  { src: "/images/shimane.jpg", matched: false, alt: "Shimane_Prefecture" },
+  { src: "/images/shizuoka.jpg", matched: false, alt: "Shizuoka_Prefecture" },
+  { src: "/images/tochigi.jpg", matched: false, alt: "Tochigi_Prefecture" },
+  { src: "/images/tokushima.jpg", matched: false, alt: "Tokushima_Prefecture" },
+  { src: "/images/tokyo.jpg", matched: false, alt: "Tokyo_Prefecture" },
+  { src: "/images/tottori.jpg", matched: false, alt: "Tottori_Prefecture" },
+  { src: "/images/toyama.jpg", matched: false, alt: "Toyama_Prefecture" },
+  { src: "/images/wakayama.jpg", matched: false, alt: "Wakayama_Prefecture" },
+  { src: "/images/yamagata.jpg", matched: false, alt: "Yamagata_Prefecture"},
+  { src: "/images/yamaguchi.jpg", matched: false, alt: "Yamaguchi_Prefecture" },
+  { src: "/images/yamanashi.jpg", matched: false, alt: "Yamanashi_Prefecture" },
 ];
+
+// wikipedia api
+const API = "https://en.wikipedia.org/api/rest_v1/page/summary/";
+
+const getPrefectureSummary = async (prefecture) => {
+  const response = await fetch(`${API}${prefecture}`);
+  const data = await response.json();
+  toast.success(data?.extract, {
+    position: "top-right",
+    autoClose: 10000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+   toastId: prefecture,
+    style: {
+     // small font size
+      fontSize: "0.8rem",
+    },
+  } );
+};
+
+
 
 function App() {
   const { width, height } = useWindowSize();
@@ -111,6 +138,10 @@ function App() {
               correct.play();
               // if it one of the cards we return it with the matched property set to true
               setMatchedCount((matchedCount += 1));
+              
+              // log the alt text of the matched cards
+              console.log(card.alt);
+              getPrefectureSummary(card.alt);
               return { ...card, matched: true };
             } else {
               return card;
@@ -150,6 +181,9 @@ function App() {
   return (
     <>
       <div className="App">
+        <ToastContainer 
+        width={"100vw"}
+        />
         <h1>Prefecture Match</h1>
         <button onClick={shuffleCards}>New Game</button>
         <div className="board">
